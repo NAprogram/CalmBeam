79d8da2945b2e3e335b1ebe709ab25b277bee242
NAprogram <NAprogram@users.noreply.github.com>
2026-09-01 13:20:49 +0400

    update: minor adjustments in home screen

--- lamp_app/lib/screens/home_screen.dart
+++ lamp_app/lib/screens/home_screen.dart
@@ -438,4 +438,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-08-29 14:41:03
+// [Auto-Sync] Diagnostic check passed at 2026-09-01 13:20:49
