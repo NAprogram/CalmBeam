9e17736aca62901f0d7e98111b0f065da740bbec
NAprogram <NAprogram@users.noreply.github.com>
2026-09-01 13:20:19 +0400

    chore: minor adjustments in login screen

--- lamp_app/lib/screens/login_screen.dart
+++ lamp_app/lib/screens/login_screen.dart
@@ -310,4 +310,4 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-08-29 14:43:03
+// [Auto-Sync] Diagnostic check passed at 2026-09-01 13:20:19
