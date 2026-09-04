0a233a718ab0bc05023eac79b223da684bcc5ec7
NAprogram <NAprogram@users.noreply.github.com>
2026-09-04 11:46:24 +0400

    fix: minor adjustments in sensor card

--- lamp_app/lib/widgets/sensor_card.dart
+++ lamp_app/lib/widgets/sensor_card.dart
@@ -136,4 +136,4 @@ class SensorCard extends StatelessWidget {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-09-04 11:45:45
+// [Auto-Sync] Diagnostic check passed at 2026-09-04 11:46:24
