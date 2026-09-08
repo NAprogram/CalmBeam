89332a127047462f55ac63ad433ab64c5d6a27f5
NAprogram <NAprogram@users.noreply.github.com>
2026-09-08 08:56:38 +0400

    refactor: minor adjustments in home screen

--- lamp_app/lib/screens/home_screen.dart
+++ lamp_app/lib/screens/home_screen.dart
@@ -438,4 +438,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-09-08 08:54:58
+// [Auto-Sync] Diagnostic check passed at 2026-09-08 08:56:38
