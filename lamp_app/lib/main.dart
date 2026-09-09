40e3752702729278c1e456095c42c3c29211f255
NAprogram <NAprogram@users.noreply.github.com>
2026-09-09 10:58:07 +0400

    refactor: minor adjustments in main

--- lamp_app/lib/main.dart
+++ lamp_app/lib/main.dart
@@ -67,4 +67,4 @@ class _AuthGate extends StatelessWidget {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-09-09 10:54:43
+// [Auto-Sync] Diagnostic check passed at 2026-09-09 10:58:07
