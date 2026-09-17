6cabbad2182c21095ddb6fe2b573a254ba15f62b
NAprogram <NAprogram@users.noreply.github.com>
2026-09-17 08:56:41 +0400

    refactor: minor adjustments in login screen

--- lamp_app/lib/screens/login_screen.dart
+++ lamp_app/lib/screens/login_screen.dart
@@ -310,4 +310,4 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-// [Auto-Sync] Diagnostic check passed at 2026-09-16 10:48:22
+// [Auto-Sync] Diagnostic check passed at 2026-09-17 08:56:40
